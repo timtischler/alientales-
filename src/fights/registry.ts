816d455de97d3b3f49a98b67f8350a6d00aa1b1c
Tim Tischler <tischler@purplecoffeecup.com>
2026-06-20 17:44:55 -0700

    feat: add Saucer Ring to the fight registry

--- src/fights/registry.ts
+++ src/fights/registry.ts
@@ -1,5 +1,6 @@
 import type { FightDefinition } from "./types";
 import { UFO_INVASION } from "./ufoInvasion";
 import { EYE_BEAMS } from "./eyeBeams";
+import { SAUCER_RING } from "./saucerRing";
 
-export const FIGHTS: readonly FightDefinition<unknown>[] = [UFO_INVASION, EYE_BEAMS];
+export const FIGHTS: readonly FightDefinition<unknown>[] = [UFO_INVASION, EYE_BEAMS, SAUCER_RING];
